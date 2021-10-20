9a52f05349c619dc44b06d35cbad08e8e3865e1e
Suje2021 <80900194+Suje2021@users.noreply.github.com>
2021-10-20 15:28:53 +0200

    függvénnyel vonalkövetés

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+function vonal (balv: number, jobbv: number) {
+    if (balv == 0 && jobbv == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+    } else if (balv == 1 && jobbv == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+    } else if (balv == 0 && jobbv == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 20)
     }
+}
+basic.forever(function () {
+    vonal(maqueen.readPatrol(maqueen.Patrol.PatrolLeft), maqueen.readPatrol(maqueen.Patrol.PatrolRight))
 })
